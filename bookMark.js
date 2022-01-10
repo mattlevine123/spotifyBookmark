7899e63bb45e82cecee6a5c176761b3cfce778ff
Matt <mattlevine123@gmail.com>
2022-01-09 21:54:18 -0500

    End points

---
+++ bookMark.js
@@ -0,0 +1,9 @@
+//get device id
+//end point https://api.spotify.com/v1/me/player/devices 
+
+//get position data
+// end point https://api.spotify.com/v1/me/player
+
+
+//set playback times stamp
+//end point https://api.spotify.com/v1/me/player/seek
